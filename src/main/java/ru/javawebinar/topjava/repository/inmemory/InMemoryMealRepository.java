864a44f4598c9f18ffd2edb3f88f8b9042a8cc18
Alexander Fomin <afomin@mail.ru>
2022-06-26 11:04:20 +0400

    HW02 1 - 3 (non thread-safe)

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
@@ -5,47 +5,67 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
-import ru.javawebinar.topjava.util.MealsUtil;
+import ru.javawebinar.topjava.web.SecurityUtil;
 
-import java.util.Collection;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+import static ru.javawebinar.topjava.util.MealsUtil.meals;
 
 @Repository
 public class InMemoryMealRepository implements MealRepository {
     public static final Logger log = LoggerFactory.getLogger(InMemoryMealRepository.class);
-    private final Map<Integer, Meal> repository = new ConcurrentHashMap<>();
+    //    каждый конкретный ==аутентифицированный== пользователь
+    //    реализовать хранение еды для каждого пользователя можно с добавлением поля userId в Meal ИЛИ без него
+    private final Map<Integer, Set<Meal>> repository = new ConcurrentHashMap<>();
     private final AtomicInteger counter = new AtomicInteger();
+    private final Set<Meal> userMeals = new TreeSet<>(Comparator.comparing(Meal::getId));
 
     {
-        MealsUtil.meals.forEach(this::save);
+        meals.forEach(meal -> save(SecurityUtil.authUserId(), meal));
     }
 
     @Override
-    public Meal save(Meal meal) {
+    public Meal save(Integer userId, Meal meal) {
         if (meal.isNew()) {
+            log.debug("save: create for user {} {}", userId, meal);
             meal.setId(counter.incrementAndGet());
-            repository.put(meal.getId(), meal);
-            return meal;
+        } else {
+            log.debug("save: update for user {} {}", userId, meal);
+            userMeals.remove(meal);
         }
-        // handle case: update, but not present in storage
-        return repository.computeIfPresent(meal.getId(), (id, oldMeal) -> meal);
+        userMeals.add(meal);
+        repository.put(userId, userMeals);
+        return meal;
     }
 
     @Override
-    public boolean delete(int id) {
-        return repository.remove(id) != null;
+    public boolean delete(Integer userId, int id) {
+        boolean removed = userMeals.remove(findMealById(userMeals, id));
+        repository.put(userId, userMeals);
+        return removed;
     }
 
     @Override
-    public Meal get(int id) {
-        return repository.get(id);
+    public Meal get(Integer userId, int id) {
+        return findMealById(repository.get(userId), id);
     }
 
     @Override
-    public Collection<Meal> getAll() {
-        return repository.values();
+    public List<Meal> getAll(Integer userId) {
+        log.debug("getAll for user {}", userId);
+        return repository.get(userId).stream()
+                .sorted(Comparator.comparing(Meal::getDateTime).reversed())
+                .collect(Collectors.toList());
+    }
+
+    private Meal findMealById(Set<Meal> meals, int id) {
+        return meals.stream()
+                .filter(meal -> meal.getId() == id)
+                .findFirst()
+                .orElse(null);
     }
 }
 
